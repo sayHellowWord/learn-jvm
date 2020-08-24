50773bab53d66927cbcf7ef810fb812df3e8a1c4
wubo15 <wubo15@meituan.com>
2020-08-24 21:26:02 +0800

    jvm  类加载

---
+++ src/main/java/com/xiaowo/learn/jvm/Singleton.java
@@ -0,0 +1,33 @@
+package com.xiaowo.learn.jvm;
+
+/**
+ * Created by wubo15 on 2020/8/24.
+ *
+ * @author wubo15
+ * @date 2020/8/24
+ */
+public class Singleton {
+
+    private Singleton() {
+    }
+
+    private static class LazyHolder {
+        static final Singleton INSTANCE = new Singleton();
+
+        static {
+            System.out.println("LazyHolder.");
+        }
+    }
+
+    public static Object getInstance(boolean flag) {
+        if (flag) return new LazyHolder[2];
+        return LazyHolder.INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        getInstance(true);
+        System.out.println("----");
+        getInstance(false);
+    }
+
+}
